146e960bd4257c93d36b7de3b8115344a1566533
Yusup Maulana <yusup.maulana@kokatto.com>
2024-06-01 23:47:02 +0700

    update project picture

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -64,13 +64,11 @@ export default function Contact() {
           </div>
         </div>
         <form
-          method="POST"
-          data-netlify="true"
+          netlify="true"
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
         >
-          <input type="hidden" name="form-name" value="contact" />
           <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
